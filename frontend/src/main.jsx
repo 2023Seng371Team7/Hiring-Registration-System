f01286d7d7eaff028c1e156d7cd46e1cd58c5bbd
kevin <kmathew@peloton-technologies.com>
2023-02-10 11:48:21 -0800

    removing unnecessary stuff

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Homepage } from './pages/Homepage'
 import LoginPage from './pages/Login';
 
-//TODO: Make actual other pages and import them
-const Page1 = () => <h1>Page 1</h1>;
-const Page2 = () => <h1>Page 2</h1>;
-const Page3 = () => <h1>Page 3</h1>;
 
 const Router = () => (
 	<Routes>
 		<Route index element={<LoginPage />} />
-		<Route path="/page1" element={<Page1 />} />
-		<Route path="/page2" element={<Page2 />} />
-		<Route path="/page3" element={<Page3 />} />
 	</Routes>
 )
 
