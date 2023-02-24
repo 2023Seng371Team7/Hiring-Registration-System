f5b3abe6bc0dbf75e65c981e33dc01f38c51b7b0
Albie Samson <albie0019@gmail.com>
2023-02-24 10:11:28 -0800

    Added test case for accessing DB

--- backend/apis/tests.py
+++ backend/apis/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+import unittest
+from utils import get_db_handle
+
 
 # Create your tests here.
+class MongoTests(unittest.TestCase):
+    def test_WriteToTestDatabase(self) -> None:
+        db, _ = get_db_handle("HRStest")
+
+        testCollection = db['TestCollection']
+        testItem = {
+            "tid": "1",
+            "Name": "admin",
+            "password": "newadmin"
+        }
+        testCollection.insert_one(testItem)
+        dbItem = testCollection.find_one({"tid": "1"})
+        self.assertDictEqual(testItem, dbItem)
+
+    def tearDown(self) -> None:
+        db, _ = get_db_handle("HRStest")
+        testCollection = db['TestCollection']
+        testCollection.delete_one({"tid": "1"})
+
--- backend/utils.py
+++ backend/utils.py
@@ -4,7 +4,6 @@ import os
 
 
 def get_db_handle(db_name):
-    #client = MongoClient('mongodb+srv://Developer:seng371@hrscluster.1wbzyii.mongodb.net/test')
     load_dotenv(find_dotenv())
     client = MongoClient(os.environ['MongoConnectionString'])
     db_handle = client[db_name]
