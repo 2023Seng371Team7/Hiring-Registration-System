81427ec093f20b0e2ad7ba63c4e5a0e26c4a09a8
kevin <kmathew@peloton-technologies.com>
2023-03-01 23:50:54 -0800

    adding jobListing frontend stuff

---
+++ frontend/src/pages/Job.jsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./Job.css";
+const App = () => {
+    return (
+        <div className="post-3">
+            <div className="rectangle-3">
+                <span className="software-testing-eng">Software Testing Engineer</span>
+                <span className="amazon">Amazon</span>
+                <span className="vancouver-bc">Vancouver, BC</span>
+                <span className="num-35-an-hour">$35 an hour</span>
+            </div>
+        </div>
+    );
+};
+export default App;
