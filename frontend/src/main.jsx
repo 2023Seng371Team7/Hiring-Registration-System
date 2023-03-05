ae9d048d17dcf462359ada6e44d15d418c66bf23
Lydia <rmlulie@uvic.ca>
2023-03-05 13:14:21 -0800

    Updated UI for Job Postings

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import JobListing from './pages/JobListings';
 import LoginPage from './pages/Login';
 import SignUpPage from './pages/SignUp';
 import myRoutes from './routes';
@@ -10,7 +11,7 @@ const Router = () => (
 	<Routes>
 		<Route path={myRoutes.login} index element={<LoginPage />} />
 		<Route path={myRoutes.SignUp} index element={<SignUpPage />} />
-
+		<Route path={myRoutes.JobsListed} index element={<JobListing />} />
 	</Routes>
 )
 
