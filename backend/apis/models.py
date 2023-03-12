be004a277ffe5bf9675d7c9fc8ed359cec8df16c
kevin <kmathew@peloton-technologies.com>
2023-03-11 23:17:19 -0800

    adding job Listing get methods

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -13,3 +13,26 @@ class User:
         self.role = role
         self.salt = salt
         self.hashed_password = hashed_password
+
+# create model for job listing
+
+
+class JobListing:
+    id: int
+    title: str
+    description: str
+    location: str
+    salary: str
+    company: str
+    date_posted: str
+    url: str
+
+    def __init__(self, id, title, description, location, salary, company, date_posted, url):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.location = location
+        self.salary = salary
+        self.company = company
+        self.date_posted = date_posted
+        self.url = url
---
+++ backend/apis/views/job_views.py
@@ -0,0 +1,35 @@
+# create a job view endpoint using django rest framework
+# create get and post methods for the job view endpoint
+# create using similiar structure of auth_views.py and manager_views.py
+# follow job model from models.py
+
+from backend.apis import models
+from backend.utils import get_db_handle
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class JobListing(APIView):
+    # GET
+    # Create method to Send all jobs using a job model array through GET API request  based on model JobListings.
+    def get(self, request):
+        db, _ = get_db_handle("HRS")
+        collection = db['job_listings']
+        jobs = collection.find()
+        job_list = []
+        for job in jobs:
+            job_list.append(models.JobListing(job['id'], job['title'], job['description'],
+                            job['location'], job['salary'], job['company'], job['date_posted'], job['url']))
+        return Response(job_list, status=status.HTTP_200_OK)
+
+    # GET
+    # Gets a specific job based on the id of the job
+    def getJob(self, request):
+        db, _ = get_db_handle("HRS")
+        collection = db['job_listings']
+        jobs = collection.find()
+        for job in jobs:
+            if job['id'] == request.data['id']:
+                return Response(job, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
