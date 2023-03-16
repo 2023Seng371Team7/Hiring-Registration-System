159ac27b9486ed41a451bcb9b915681d4cbce181
kevin <kmathew@peloton-technologies.com>
2023-03-16 16:25:26 -0700

    fixing imports

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from apis.views.auth_views import user_authenticate
 from apis.views.manager_views import ManagerUpdate
 from apis.views.user_views import applications, relevantpostings
-from backend.apis.views.jobs_views import JobListings
+from apis.views.jobs_views import JobListings
 
 urlpatterns = [
     path("managerupdate", ManagerUpdate.as_view()),
