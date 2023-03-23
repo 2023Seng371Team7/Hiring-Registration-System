511b655b8194efbdddcfc7ac633329c549f388ea
kevin <kmathew@peloton-technologies.com>
2023-03-22 23:43:41 -0700

    adding applications API

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -3,6 +3,8 @@ from apis.views.auth_views import user_authenticate
 from apis.views.manager_views import ManagerUpdate
 from apis.views.user_views import applications, relevantpostings
 from apis.views.job_views import Job
+from apis.views.jobApplication_views import JobApplication
+from apis.views.updateApplication_views import UpdateApplication
 from apis.views.jobListings_views import JobListings
 
 urlpatterns = [
@@ -10,6 +12,8 @@ urlpatterns = [
     path("login", user_authenticate),
     path("joblisting", JobListings.as_view()),
     path("job", Job.as_view()),
+    path('jobApplications', JobApplication.as_view()),
+    path('updateApplication', UpdateApplication.as_view()),
     path('applications', applications),
     path('relevantpostings', relevantpostings),
 ]
